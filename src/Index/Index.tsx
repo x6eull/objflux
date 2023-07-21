776096c91faa95886e6e39a1bb1d2760cbdc820a
Passthem <514827965@qq.com>
2023-07-21 17:00:50 +0800

    Update something for <Input />

--- src/Index/Index.tsx
+++ src/Index/Index.tsx
@@ -4,16 +4,20 @@ import { Checkbox } from '../Checkbox/Checkbox';
 
 import '../tool/Tool';
 import { Input } from '../Input/Input';
-import { useState } from 'react';
 
 export function IndexElement() {
-  const [stringValue, setStringValue] = useState<string>('a');
-
   return (<><Folder title='可见性' desc='修改可见性不会更改工具的链接' >
     <OptionGroup options={[{ title: '公开', desc: '任何人都可使用此工具' }, { title: '仅链接', desc: '此工具不显示在探索页或您的公开工具中，但通过链接仍可使用，用户也可以收藏' }, { title: '私有', desc: '仅您可使用此工具' }]} /></Folder><Folder title='复刻权限' >
       <OptionGroup options={[{ title: '所有人', desc: '所有登录用户均可复刻此工具' }, { title: '需要申请', desc: '用户可以申请复刻此工具' }, { title: '仅自己' }]} /><Checkbox title='保留复刻来源' desc='其他用户复刻此工具后，复刻的工具将默认显示复刻来源'></Checkbox></Folder>
-    <Folder> <Folder> <Folder> <Folder><OptionGroup options={['x1', 'x2']} /></Folder></Folder> </Folder> </Folder>
+    <Folder>
+      <Folder> <Folder> <Folder><OptionGroup options={['x1', 'x2']} /></Folder></Folder> </Folder>
+      <Checkbox title='Checkbox Without Description' />
+      <Input title='Input Without Description' placeholder='Form elements must have labels: Element has no title attribute Element has an empty placeholder attribute' />
+      <Input title='Input With Description' seperateTitleWithInput={true} desc='Description Texts. 有理有理，哈吉马路油！' />
+      <Input title='哎呀这不' desc='无论如何，输入框一定会给你报错' error={true} placeholder='爆！' />
+      <Input allowMultiline={true} title='换行爱上对方过后就哭了什么时候开发OPAI' desc='全部换行，Passthem 欢迎回到 TopZeroUnion 今天是开服的第 6.02 × 10²³ 天' seperateTitleWithDescription={true} seperateTitleWithInput={true} placeholder='不丑吗' />
+      <Input title='开始写多行了兄弟萌' desc='(๑• . •๑) 明白了w' allowMultiline={true} placeholder='马上就来啦' />
+    </Folder>
     <OptionGroup options={['x1', 'x2']} />
-    <Input title='阿斯顿法国红酒快乐' value={stringValue} onChange={value => {setStringValue(value)}} />
   </>);
 }
--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -7,23 +7,48 @@ import { FolderLevelContext } from '../Folder/FolderLevelContext';
  * 此控件可以受控（指定`value`则受控）
  */
 export function Input(
-    { title, desc, initialValue, value, onChange, type }:
-        { title: string, desc?: string, initialValue?: string, value?: string, onChange?: (value: string) => void, type?: 'text' | 'number' }
+    { title, desc, initialValue, value, onChange, allowMultiline, placeholder, error, seperateTitleWithInput, seperateTitleWithDescription, minViewLines, maxViewLines }:
+        { title: string, desc?: string, initialValue?: string, value?: string, onChange?: (value: string) => void, allowMultiline?: boolean, placeholder?: string, error?: boolean, seperateTitleWithInput?: boolean, seperateTitleWithDescription?: boolean, minViewLines?: number, maxViewLines?: number }
 ) {
-    type = type ? type : 'text';
+    allowMultiline ??= false;
+    seperateTitleWithInput ??= false;
+    placeholder ??= '';
+    minViewLines ??= 1;
+    maxViewLines ??= 5;
+
+    minViewLines = Math.max(minViewLines, 1);
+    maxViewLines = Math.max(maxViewLines, minViewLines);
+
     const level = useContext(FolderLevelContext);
     const [stateValue, setStateValue] = useState<string>(initialValue ?? '');
     value ??= stateValue;
-    return <div style={{ paddingLeft: `${(level + 1) * 1.2}rem` }}>
-        <div className='text'>
+    return <div style={{ paddingLeft: `${(level + 1) * 1.2}rem` }} className={'input' + (allowMultiline ? ' multiline-input' : '') + (seperateTitleWithInput ? ' seperated-input' : '')}>
+        <div className={'text' + (seperateTitleWithDescription ? ' seperated-text' : '')}>
             <div className='title'>{title}</div>
             {desc ? (<div className='desc'>{desc}</div>) : (<></>)}
         </div>
-        <input type={type} value={value} onChange={e => {
-                setStateValue(e.target.value);
-                onChange?.(e.target.value ?? '');
+        {allowMultiline ? (<textarea placeholder={placeholder} className={'textarea' + (error ? ' error' : '')} onChange={e => {
+            let _result = e.target.value;
+            _result = allowMultiline ? _result : _result.replace(/\n/g, '');
 
-                console.log(e.target.value);
-            }} />
+            setStateValue(_result);
+            onChange?.(_result ?? '');
+        }} style={{
+            height: `${(Math.max(
+                Math.min(
+                    value.length - value.replace(/\n/g, '').length + 1,
+                    maxViewLines
+                ),
+                minViewLines
+            )) * 1.5}em`
+        }}>
+            {value}
+        </textarea>) : (<input placeholder={placeholder} className={'textarea' + (error ? ' error' : '')} value={value} onChange={e => {
+            let _result = e.target.value;
+            _result = allowMultiline ? _result : _result.replace(/\n/g, '');
+
+            setStateValue(_result);
+            onChange?.(_result ?? '');
+        }} />)}
     </div>;
 }
