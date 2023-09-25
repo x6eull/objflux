bceaef89c86ecdb70f2f5e3ea239382b6a4b9702
x6eull <x6eull@gmail.com>
2023-09-25 21:08:02 +0800

    Fix unused var

--- src/utils/PointerManager.tsx
+++ src/utils/PointerManager.tsx
@@ -2,7 +2,7 @@ import React, { useMemo } from 'react';
 import { scopedVar } from './utils';
 
 export function PointerManager({ children }: { children: React.ReactNode }) {
-  const { get: getOn, set: setOn } = useMemo(() => scopedVar(false), []);
+  const { get: _getOn, set: setOn } = useMemo(() => scopedVar(false), []);
   return <div onPointerDown={() => {
     setOn(true);
   }}>{children}</div>;
