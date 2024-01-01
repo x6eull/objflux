d30913ba113499e7a35be13f1e4d9c259cfba798
x6eull <x6eull@gmail.com>
2024-01-01 19:52:29 +0800

    Add miniFolderIcon option for Folder

--- src/Folder/Folder.tsx
+++ src/Folder/Folder.tsx
@@ -8,21 +8,33 @@ import { FolderLevelContext } from './FolderLevelContext';
  * 此组件不受控，除第一次渲染外，`initialExpanded`会被忽略。
  */
 export function Folder
-  ({ title = '选项', desc, children, initialExpanded = false, onChange }: { title?: string, desc?: string, children?: React.ReactNode, initialExpanded?: boolean, onChange?: (expanded: boolean) => void }) {
+  ({ title = '未命名组', desc, children, initialExpanded = false, onChange, miniFolderIcon = false }: { title?: string, desc?: string, children?: React.ReactNode, initialExpanded?: boolean, onChange?: (expanded: boolean) => void, miniFolderIcon?: boolean }) {
   const [expanded, setExpanded] = useState<boolean>(initialExpanded);
   const level = useContext(FolderLevelContext);
+  function toggleExpanded(ev: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    ev.stopPropagation();
+    ev.preventDefault();
+    const e = !expanded;
+    setExpanded(e);
+    onChange?.(e);
+  }
   return (
     <div className={'folder' + (level > 0 ? ' nested' : '')}>
-      <div style={level > 0 ? { paddingLeft: `${(level + 1) * 1.2}rem` } : {}} className={'control-card' + (expanded ? ' expanded' : '')} onClick={() => {
-        const e = !expanded;
-        setExpanded(e);
-        onChange?.(e);
+      <div style={level > 0 ? { paddingLeft: `${(level + 1) * 1.2}rem` } : {}} className={'control-card'
+        + (expanded ? ' expanded' : '')
+        + (miniFolderIcon ? ' mini-fold' : '')
+      } onClick={(ev) => {
+        if (miniFolderIcon)
+          return;
+        toggleExpanded(ev);
       }}>
         <div className='text'>
           <div className='title'>{title}</div>
           {desc ? (<div className='desc'>{desc}</div>) : undefined}
         </div>
-        <div className={'icon' + (expanded ? ' expanded' : '')}><FolderIcon className='inner' /></div>
+        <div onClick={(ev) => {
+          toggleExpanded(ev);
+        }} className={'icon' + (expanded ? ' expanded' : '')}><FolderIcon className='inner' /></div>
       </div>
       <div className={'content-card' + (expanded ? ' expanded' : '')}>
         <FolderLevelContext.Provider value={level + 1}>
--- src/Folder/FolderIcon.tsx
+++ src/Folder/FolderIcon.tsx
@@ -2,7 +2,7 @@ import { SVGProps } from 'react';
 
 export function FolderIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox='0 0 100 100' stroke='#757575' strokeWidth={4} {...props}>
+    <svg viewBox='0 0 100 100' stroke='#757575' strokeWidth={6} {...props}>
       <line x1={0} y1={25} x2={50} y2={75} />
       <line x1={100} y1={25} x2={50} y2={75} />
     </svg>);
